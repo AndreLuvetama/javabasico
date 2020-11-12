fe1eb645b2e7982ac932b1fb5e2af7e2d2cba815
AndreLuvetama <andreluares30@gmail>
2020-11-12 16:14:19 -0300

    novos arquivos

--- PrimeiraSemanaJava/src/org/com/aulajava/OperadorWhile.java
+++ PrimeiraSemanaJava/src/org/com/aulajava/OperadorWhile.java
@@ -7,18 +7,19 @@ public class OperadorWhile {
 //Criar uma estrutura com While que imprimi os dados enquanto o valor for diferente de c for C
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nome =" ";
+		String controle =" ";
 		
 		Scanner  teclado = new Scanner(System.in);
 		Scanner  teclado2 = new Scanner(System.in);
 		
-		while(!nome.equals("c") ) {
-			System.out.print("Digite o nome\n");
-			 nome = teclado.nextLine();
-			System.out.print("Digite a Idade\n");
+		while(!controle.equals("c") ) {
+			System.out.println("Digite o nome");
+			 controle = teclado.nextLine();
+			System.out.println("Digite a Idade");
 			int idade = teclado2.nextInt();
-			System.out.print("Digite peso\n");
+			System.out.println("Digite peso");
 			float peso = teclado2.nextFloat();
+			System.out.printf("\nMeu nome é %s \nMinha  idade é %d  \nMeu peso é %.2f \n", controle, idade, peso); 
 			
 		}
 		
