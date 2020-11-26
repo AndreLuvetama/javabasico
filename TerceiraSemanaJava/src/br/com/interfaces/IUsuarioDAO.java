03455ef4a7de0c011c92405608ae3f70e76dd15e
AndreLuvetama <andreluares30@gmail.com>
2020-11-26 15:01:13 -0300

    Terceira Semana QF

---
+++ TerceiraSemanaJava/src/br/com/interfaces/IUsuarioDAO.java
@@ -0,0 +1,24 @@
+package br.com.interfaces;
+
+
+
+import java.util.List;
+
+import br.com.models.Usuario;
+
+public interface IUsuarioDAO {
+
+	Usuario buscarUsuario(Long id);
+	
+	List<Usuario> buscarUsuarios();
+
+	Integer cadastrarUsuario(Usuario u, String senha);
+	
+	Boolean verificarUsuarioESenha(String usuario, String senha);
+	
+	Integer deletarUsuario(Long id);
+		
+	Integer editarUsuario(Usuario u);
+	
+	List<Usuario> buscarUsuariosPorParteDoNome(String nome);
+}
